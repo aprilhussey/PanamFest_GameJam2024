f36b38e8015cc717c79497facf89e60979bc3935
April Hussey <april.hussey@outlook.com>
2024-01-16 14:41:15 +0000

    Added AudioManager script and gameObject

---
+++ PanamFest2024/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        foreach (Sound sound in sounds)
+        {
+            sound.source.gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch= sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound sound = Array.Find(sounds, sound => sound.name == name);
+        if (sound == null)
+        {
+            Debug.Log($"Sound {name} not found");
+            return;
+        }
+		sound.source.Play();
+	}
+}
