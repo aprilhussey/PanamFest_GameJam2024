48a2390daff63ffafa2a87fcd0306ed052a1316c
CptInori <128365775+CptInori@users.noreply.github.com>
2024-01-16 11:26:40 +0000

    Content Added
    - Updated the enemy prefab so they now die when shot.
    - GameManager added, contains 3 variables relating to score and earning points.
    - Scoring scripted added, works on the enemies, but could be modified.
    - Player prefab added, hasn't been tested so unpack before use.
    - Changes made to Ben's Scene
    - Some temp materials added (can be deleted).
    - Altered the vfxHit particle system so it now destroys itself on completion.

---
+++ PanamFest2024/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Scores applied when landing a hit
+    // Standard body shot
+    [Tooltip("The score added for a body shot")]
+    public int NormalScore;
+
+    // Headshot/Critical strike
+    [Tooltip("The score added for a critical hit")]
+    public int CritScore;
+
+    // Player's total score
+    [Tooltip("The players total score (shoudln't need touching)")]
+    public int PlayerScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PanamFest2024/Assets/Scripts/Scoring.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    [SerializeField] GameManager GameManager;
+
+    [Tooltip("The crit box attatched to the enemy")]
+    [SerializeField] GameObject CritHitBox;
+
+    [Tooltip("The body attached to the enemy")]
+    [SerializeField] GameObject BodyHitBox;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CritHitBox == null || BodyHitBox == null)
+        {
+            if (CritHitBox == null)
+            {
+                GameManager.PlayerScore += GameManager.CritScore;
+            }
+
+            else if (BodyHitBox == null)
+            {
+                GameManager.PlayerScore += GameManager.NormalScore;
+            }
+
+            Destroy(this.gameObject);
+
+        }
+    }
+}
