361552db478fc34233c004fbb1f3a62cbe11fdc9
kirsty09 <kirsty.mcglynn21@my.northampton.ac.uk>
2024-01-16 09:54:32 +0000

    Added Substance 3D Plugin
    Substance files can now be directly put into to project.

---
+++ PanamFest2024/Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
@@ -0,0 +1,9 @@
+namespace Adobe.Substance
+{
+    public static class Version
+    {
+        public const string PluginVersion = "3.10.0-16-g4a9bc1b";
+        public const string EngineVersion = "9.0.0 537e2e31";
+        public const int ImporterVersion = 3100;
+    }
+}
