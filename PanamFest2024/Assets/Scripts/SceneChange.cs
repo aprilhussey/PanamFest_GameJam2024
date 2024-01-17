c5a8cf0e1e101232350366fe6cca29d3f4ef510e
CptInori <128365775+CptInori@users.noreply.github.com>
2024-01-17 10:30:16 +0000

    Added: New Script
    New script added, Scene Change, to allow scene transitions involving a loading scene

---
+++ PanamFest2024/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [Tooltip("The name of the loading scene")]
+    [SerializeField] string LoadingScene;
+
+    [Tooltip("The name of the scene you want to got to next")]
+    [SerializeField] string NextScene;
+
+    IEnumerator LoadNextScene()
+    {
+        AsyncOperation AsyncLoading = SceneManager.LoadSceneAsync(NextScene);
+
+        while (!AsyncLoading.isDone) // Add extra check is you want to wait for certain animations or feature to be done before the new scene loads
+        {
+            yield return null;
+        }
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(LoadingScene);
+        StartCoroutine(LoadNextScene());
+    }
+
+    public void OnLoadScene()
+    {
+        SceneManager.LoadScene(LoadingScene);
+        StartCoroutine(LoadNextScene());
+    }
+
+}
