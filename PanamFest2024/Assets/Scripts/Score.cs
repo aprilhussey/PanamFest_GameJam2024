70b63876830d6762f484c8cd83f333038de3f4e2
Jyoung77e <jackyoung808@googlemail.com>
2024-01-17 16:29:09 +0000

    sadwa

---
+++ PanamFest2024/Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreText;
+
+    private int score;
+
+    private void Awake()
+    {
+        score = GameManager.Instance.PlayerScore;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
